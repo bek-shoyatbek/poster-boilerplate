a2ac4acadb2092194431caab2333e50db574400c
bek <shoyatbekov03032003@gmail.com>
2024-06-21 18:31:53 +0500

    successfully runned but not worked

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,27 +1,24 @@
-import '../css/main.scss';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import "../css/main.scss";
+import React from "react";
+import ReactDOM from "react-dom";
 
-import HelloWorldApp from '../../examples/hello-world/app';
-import LoyaltyApp from '../../examples/loyalty/app';
-import TextPrintApp from '../../examples/text-print/app';
-import PmsApp from '../../examples/pms/App';
+import HelloWorldApp from "../../examples/hello-world/app";
+import LoyaltyApp from "../../examples/loyalty/app";
+import TextPrintApp from "../../examples/text-print/app";
+import PmsApp from "../../examples/pms/App";
 
 class ExampleApp extends React.Component {
-    render() {
-        // Чтобы отобразить нужный пример, просто закомментируйте не нужные компоненты
+  render() {
+    // Чтобы отобразить нужный пример, просто закомментируйте не нужные компоненты
 
-        return <HelloWorldApp />;
+    return <HelloWorldApp />;
 
-        return <LoyaltyApp />;
+    // return <LoyaltyApp />;
 
-        return <TextPrintApp />;
+    // return <TextPrintApp />;
 
-        return <PmsApp />;
-    }
+    // return <PmsApp />;
+  }
 }
 
-ReactDOM.render(
-    <ExampleApp />,
-    document.getElementById('app-container'),
-);
+ReactDOM.render(<ExampleApp />, document.getElementById("app-container"));
--- upload.mjs
+++ upload.mjs
@@ -1,17 +1,18 @@
-import r from 'request';
-import md5 from 'md5';
-import fs from 'fs';
-import shell from 'shelljs';
-import manifest from './manifest.json' assert { type: 'json'};
+import r from "request";
+import md5 from "md5";
+import fs from "fs";
+import shell from "shelljs";
+import manifest from "./manifest.json" assert { type: "json" };
 
-const URL = 'https://platform.joinposter.com/api/application.uploadPOSPlatformBundle?format=json';
-const FILENAME = 'bundle.js';
+const URL =
+  "https://platform.joinposter.com/api/application.uploadPOSPlatformBundle?format=json";
+const FILENAME = "bundle.js";
 
 (function () {
-  console.log('Started bundle build, you will see a message in a minute...');
+  console.log("Started bundle build, you will see a message in a minute...");
 
-  if (!shell.exec('yarn build')) {
-    console.log('Error while preparing build');
+  if (!shell.exec("yarn build")) {
+    console.log("Error while preparing build");
     return;
   }
 
@@ -23,7 +24,7 @@ const FILENAME = 'bundle.js';
           fileMd5,
           manifest.applicationSecret,
         ],
-        sign = md5(signParts.join(':'));
+        sign = md5(signParts.join(":"));
 
       const formData = {
         application_id: manifest.applicationId,
@@ -31,31 +32,34 @@ const FILENAME = 'bundle.js';
         bundle: fs.createReadStream(`./${FILENAME}`),
       };
 
-      r.post({
-        url: URL,
-        formData,
-      }, (err, response, body) => {
-        if (!err) {
-          try {
-            body = JSON.parse(body);
+      r.post(
+        {
+          url: URL,
+          formData,
+        },
+        (err, response, body) => {
+          if (!err) {
+            try {
+              body = JSON.parse(body);
 
-            if (body.error) {
-              throw new Error(JSON.stringify(body));
+              if (body.error) {
+                throw new Error(JSON.stringify(body));
+              }
+
+              console.log("Bundle successfully sent to Poster");
+            } catch (e) {
+              console.log("Error while send bundle to Poster...");
+              console.log(e);
             }
-
-            console.log('Bundle successfully sent to Poster');
-          } catch (e) {
-            console.log('Error while send bundle to Poster...');
-            console.log(e);
+          } else {
+            console.log("Error while send bundle to Poster...");
+            console.log(err);
           }
-        } else {
-          console.log('Error while send bundle to Poster...');
-          console.log(err);
         }
-      });
+      );
     } else {
       console.log(`Error while reading ${FILENAME}`);
       console.log(err);
     }
   });
-}());
+})();
